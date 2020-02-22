29cd9ea5750ef0531d1d5bd63f490933a7fce18e
Erik Scow <erikscow@gmail.com>
2020-02-21 18:27:03 -0800

    Set up project with correct dependencies.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header.js";
+
+import axios from 'axios'
+
+import WelcomePage from './components/WelcomePage'
 
 
 export default function App() {
+
+  const [characters, setCharacters] = useState([])
+
+  useEffect( () => {
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then(response => {
+        console.log(response.data)
+        setCharacters(response.data)
+      })
+      .catch(error => console.log(error))
+  }, [])
+
   return (
     <main>
       <Header />
+      <WelcomePage />
     </main>
   );
 }
