63c403de598a5611f673e20fed9cedda76533daa
Erik Scow <erikscow@gmail.com>
2020-02-21 19:27:48 -0800

    Pulled data using axios and rendered to the characters page.

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
 
+import CharacterCard from './CharacterCard'
+
 export default function CharacterList() {
 
-  const [characters, setCharacters] = useState([])
+  const [characters, setCharacters] = useState([ ])
 
   useEffect( () => {
     axios.get('https://rickandmortyapi.com/api/character/')
       .then(response => {
-        console.log(response.data)
-        setCharacters(response.data)
+        console.log('data', response.data)
+        setCharacters(response.data.results)
       })
       .catch(error => console.log(error))
   }, [])
@@ -18,7 +20,9 @@ export default function CharacterList() {
 
   return (
     <section className="character-list">
-      
+      {characters.map( character => {
+          return <CharacterCard key={character.id} {...character}/>
+      })}
     </section>
   );
 }
